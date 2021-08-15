7b73affe01101bebb1ae60f9e353bef7f09cd971
BrusEspinal <68929103+BrusEspinal@users.noreply.github.com>
2021-08-15 16:14:06 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let color = document.querySelector(".color");
+let contenedor = document.querySelector(".contenedor");
+
+color.addEventListener("click", () => {
+    contenedor.classList.toggle("color")
+});
+
